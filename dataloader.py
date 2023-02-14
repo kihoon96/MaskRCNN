6270b3ee4073334a6b6e173d691b56a1e5c0fd42
kihoon <kihoon96ss@naver.com>
2023-02-14 17:45:51 +0900

    DataloaderDone

--- dataloader.py
+++ dataloader.py
@@ -24,17 +24,20 @@ class Dataloader_COCO(torch.utils.data.Dataset):
         else:
             self.coco = COCO(test_annot_path)
             self.img_path = test_img_path
-        self.catIds = coco.getCatIds(catNms=['person'])
-        self.imgIds = coco.getImgIds(catIds=self.catIds)
+        self.catIds = self.coco.getCatIds(catNms=['person'])
+        self.imgIds = self.coco.getImgIds(catIds=self.catIds)
+
+        self.datas = self._load_data()
 
     def __getitem__(self, index):
-        img = self.coco.loadImgs(imgIds[index])[0]
+        img = self.coco.loadImgs(self.imgIds[index])[0]
         # img rgb to bgr
         img_file_path = osp.join(self.img_path, img['file_name'])
         input_img = cv2.imread(img_file_path, cv2.IMREAD_COLOR | cv2.IMREAD_IGNORE_ORIENTATION)[:,:,::-1]
 
-        annIds = self.coco.getAnnIds(imgIds=img['id'], catIds=self.catIds, iscrowd=None)
+        annIds = self.coco.getAnnIds(imgIds=img['id'], catIds=self.catIds, iscrowd=False)
         anns = self.coco.loadAnns(annIds)
+        bboxes = []
         for ann in anns:
             bboxes.append(ann['bbox'])
 
@@ -42,3 +45,7 @@ class Dataloader_COCO(torch.utils.data.Dataset):
 
     def __len__(self):
         return len(self.imgIds)
+
+    def _load_data(self):
+        datalist = []
+        
